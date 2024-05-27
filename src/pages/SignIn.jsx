3a970ba9001fe9283921d60cd6ee7105fd9ad999
Ulvi Azizli <ulviazizli96@gmail.com>
2024-05-27 18:02:56 +0200

    auth with google start

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -6,6 +6,7 @@ import {
   signInSuccess,
   signInFailure,
 } from "../redux/user/userSlice";
+import OAuth from "../components/OAuth";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
@@ -70,10 +71,11 @@ export default function SignIn() {
         />
         <button
           disabled={loading}
-          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-90 disabled:opacity-70"
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-85 disabled:opacity-70"
         >
           {loading ? "Loading" : "Sign In"}
         </button>
+        <OAuth/>
       </form>
       <div className="flex gap-2 mt-5">
         <p>Dont have an account?</p>
