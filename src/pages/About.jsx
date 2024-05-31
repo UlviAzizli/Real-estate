4f66581cc2f7dfe2326d4d9374fecd4cd0043795
Ulvi Azizli <ulviazizli96@gmail.com>
2024-05-31 09:31:39 +0200

    about page done

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-900">
+        About <span className="text-slate-700">Ironhack Estate</span>
+      </h1>
+      <div className="text-slate-700 text-xs sm:text-sm mt-4">
+        Ironhack Estate is committed to helping you find the perfect place to
+        call home.
+        <br />
+        With a diverse selection of properties, we ensure that you have a
+        variety of options to suit your unique preferences and needs.
+        <br />
+        Our dedicated team of experts is here to guide you every step of the
+        way, making the process of finding your dream home seamless and
+        enjoyable.
+      </div>
+    </div>
+  );
 }
