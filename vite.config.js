f0ad9c60acfdf526082b273ed0eca93fdfdcd75d
Ulvi Azizli <ulviazizli96@gmail.com>
2024-06-28 14:48:14 +0200

    deployment client

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: import.meta.env.VITE_BACKEND_URL || "http://localhost:5005",
+        target: process.env.VITE_BACKEND_URL || "http://localhost:5005",
         secure: false,
         changeOrigin: true,
       },
